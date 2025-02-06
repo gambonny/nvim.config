b5778e72d36bf4b4489eb65d40a49de80c81d2a8
Jonny Gamba <gambonny@gmail.com>
2025-02-06 14:17:42 -0500

    feat(plugin): window

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -5,7 +5,6 @@ vim.g.mapleader = ";"
 vim.g.maplocalleader = ";"
 
 map_key("n", "<leader>ww", "update", "Update")
-map_key("n", "<leader>f", "q!", "Close buffer w/o save")
 map_key("n", "<leader>qt", "tabclose!", "Close tab w/o save")
 map_key("n", "<leader>qq", "qall!", "Close editor w/o save")
 map_key("n", "<leader>_", "b#", "Go to last visited buffer")
