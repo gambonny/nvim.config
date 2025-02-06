1b4aa160dd2a757120c28b2f23146d764759832c
Jonny Gamba <gambonny@gmail.com>
2025-02-05 19:39:41 -0500

    feat(plugin): smart-splits

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -8,9 +8,3 @@ map_key("n", "<leader>ww", "update", "Update")
 map_key("n", "<leader>f", "q!", "Close buffer w/o save")
 map_key("n", "<leader>qt", "tabclose!", "Close tab w/o save")
 map_key("n", "<leader>qq", "qall!", "Close editor w/o save")
-
--- Custom Ctrl-w remaps
-map_key("n", "<leader>l", "wincmd l", "Map ;l to move right")
-map_key("n", "<leader>k", "wincmd k", "Map ;k to move up")
-map_key("n", "<leader>j", "wincmd j", "Map ;j to move down")
-map_key("n", "<leader>h", "wincmd h", "Map ;h to move left")
---
+++ lua/plugins/smart-splits.lua
@@ -0,0 +1,51 @@
+local utils = require("core.utils")
+
+return {
+  "mrjones2014/smart-splits.nvim",
+  config = function()
+    -- move
+    utils.map_key("n", "<leader>h", function()
+      require("smart-splits").move_cursor_left()
+    end, "move cursor left")
+    utils.map_key("n", "<leader>l", function()
+      require("smart-splits").move_cursor_right()
+    end, "move cursor right")
+    utils.map_key("n", "<leader>j", function()
+      require("smart-splits").move_cursor_down()
+    end, "move cursor down")
+    utils.map_key("n", "<leader>k", function()
+      require("smart-splits").move_cursor_up()
+    end, "move cursor up")
+    utils.map_key("n", "<leader>1r", function()
+      require("smart-splits").move_cursor_previous()
+    end, "move cursor up")
+
+    -- resize
+    utils.map_key("n", "<leader>@r", function()
+      require("smart-splits").resize_left(15)
+    end, "resize cursor left")
+    utils.map_key("n", "<leader>6r", function()
+      require("smart-splits").resize_right(15)
+    end, "resize cursor right")
+    utils.map_key("n", "<leader>4r", function()
+      require("smart-splits").resize_down(15)
+    end, "resize cursor down")
+    utils.map_key("n", "<leader>5r", function()
+      require("smart-splits").resize_up(15)
+    end, "resize cursor up")
+
+    -- swap
+    utils.map_key("n", "<leader>@s", function()
+      require("smart-splits").swap_buf_left()
+    end, "swap buffer left")
+    utils.map_key("n", "<leader>6s", function()
+      require("smart-splits").swap_buf_right()
+    end, "swap buffer right")
+    utils.map_key("n", "<leader>4s", function()
+      require("smart-splits").swap_buf_down()
+    end, "swap buffer down")
+    utils.map_key("n", "<leader>5s", function()
+      require("smart-splits").swap_buf_up()
+    end, "swap buffer up")
+  end,
+}
