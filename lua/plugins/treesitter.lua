57c888ecef84dc9aa313e7fe1fda191a84089679
Jonny Gamba <gambonny@gmail.com>
2025-02-06 16:17:44 -0500

    feat(plugin): treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = { "lua", "vim", "bash", "javascript", "typescript", "json", "html", "css", "astro" },
+      highlight = { enable = true },
+      indent = { enable = true },
+      incremental_selection = { enable = true },
+    })
+  end
+}
+
