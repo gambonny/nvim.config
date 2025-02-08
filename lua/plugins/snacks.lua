b04b603bb749100df3df1ee55d86d123452f6908
Jonny Gamba <gambonny@gmail.com>
2025-02-07 19:24:37 -0500

    feat(picker): picker.buffers

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -12,6 +12,30 @@ return {
   },
   keys = {
     { "<leader>'", function() Snacks.explorer({ git_status_open = true }) end, desc = "File Explorer" },
-    { "<leader><leader>", function() Snacks.picker.files() end, desc = "Find Files" }
+    { "<leader><leader>", function() Snacks.picker.files({
+      finder = "files",
+      format = "file",
+      show_empty = true,
+      supports_live = true,
+      layout = 'ivy',
+    }) end, desc = "Find Files" },
+    { '<leader>,',  function()
+      Snacks.picker.buffers({
+        finder = "buffers",
+        format = "buffer",
+        hidden = false,
+        unloaded = true,
+        current = true,
+        sort_lastused = true,
+          win = {
+            input = {
+              keys = {
+                ["d"] = "bufdelete",
+              },
+            },
+            list = { keys = { ["d"] = "bufdelete" } },
+          },
+      layout = "vscode",
+    }) end, desc = "[P]Snacks picker buffers", },
   }
 }
