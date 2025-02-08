4401eb3d9e2c0204c0c6f14cab90a9fdc528ebf7
Jonny Gamba <gambonny@gmail.com>
2025-02-08 10:03:00 -0500

    feat(blink): show signatures and documentation

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,8 +1,6 @@
 return {
   "saghen/blink.cmp",
-
   version = "*",
-
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
@@ -16,6 +14,40 @@ return {
       -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
       -- Adjusts spacing to ensure icons are aligned
       nerd_font_variant = "mono",
+      kind_icons = {
+        Text = "",
+        Method = "",
+        Function = "",
+        Constructor = "",
+        Field = "",
+        Variable = "",
+        Class = "",
+        Interface = "",
+        Module = "",
+        Property = "",
+        Unit = "",
+        Value = "",
+        Enum = "",
+        Keyword = "",
+        Snippet = "",
+        Color = "",
+        File = "",
+        Reference = "",
+        Folder = "",
+        EnumMember = "",
+        Constant = "",
+        Struct = "",
+        Event = "",
+        Operator = "",
+        TypeParameter = "",
+      },
+    },
+
+    signature = {
+      enabled = true,
+      window = {
+        show_documentation = true,
+      },
     },
 
     -- Default list of enabled providers defined so that you can extend it
@@ -24,8 +56,19 @@ return {
       default = { "lsp", "path", "snippets", "buffer" },
     },
     completion = {
+      list = {
+        selection = {
+          preselect = true,
+          auto_insert = true,
+        },
+      },
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 500,
+      },
       menu = {
         draw = {
+          treesitter = { "lsp" },
           -- We don't need label_description now because label and label_description are already
           -- combined together in label by colorful-menu.nvim.
           columns = { { "kind_icon" }, { "label", gap = 1 } },
