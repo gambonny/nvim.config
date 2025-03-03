1b85a064ac792849aca9b22559d732f7e46bf799
Jonny Gamba <gambonny@gmail.com>
2025-03-03 16:54:20 -0500

    feat(plugin): goto-preview

---
+++ lua/plugins/goto-preview.lua
@@ -0,0 +1,43 @@
+local utils = require("core.utils")
+
+return {
+  "rmagatti/goto-preview",
+  event = "VeryLazy",
+  config = function()
+    require("goto-preview").setup({
+      width = 140,
+      height = 25,
+    })
+
+    utils.map_key(
+      "n",
+      "<leader>gpd",
+      "lua require('goto-preview').goto_preview_definition()",
+      "Go to preview definition"
+    )
+    utils.map_key(
+      "n",
+      "<leader>gpt",
+      "lua require('goto-preview').goto_preview_type_definition()",
+      "Go to preview definition"
+    )
+    utils.map_key(
+      "n",
+      "<leader>gpi",
+      "lua require('goto-preview').goto_preview_implementation()",
+      "Go to preview definition"
+    )
+    utils.map_key(
+      "n",
+      "<leader>gpD",
+      "lua require('goto-preview').goto_preview_declaration()",
+      "Go to preview definition"
+    )
+    utils.map_key(
+      "n",
+      "<leader>gpr",
+      "lua require('goto-preview').goto_preview_references()",
+      "Go to preview definition"
+    )
+  end,
+}
