b25b8e2f453b49b9fb50cb49a7a99849eb8ca607
Jonny Gamba <gambonny@gmail.com>
2026-05-04 18:09:15 -0500

    add flouride key maps

--- lua/plugins/flouride.lua
+++ lua/plugins/flouride.lua
@@ -1,6 +1,11 @@
+local utils = require("core.utils")
+
 return {
   "Sang-it/fluoride",
   config = function()
     require("fluoride").setup()
+
+    utils.map_key("n", "<leader>cp", ":Fluoride", "Fluoride")
+    utils.map_key("n", "<leader>cv", ":Fluoride vsplit", "Fluoride split")
   end,
 }
